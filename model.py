427d3170afa2b09c0bd9f545f761c7aa69e326c8
Ron Snir <ron.snir@klarna.com>
2020-08-14 23:57:24 +0200

    changed path to load the data

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import joblib
 
 
 #get the data
-df = pd.read_csv('/Users/ron.snir/Documents/python_test/dataset.csv', sep=';')
+df = pd.read_csv('/home/ubuntu/aws/dataset.csv', sep=';')
 
 #split the data for train (exclude test)
 df_train = df[df['default'].notnull()]
